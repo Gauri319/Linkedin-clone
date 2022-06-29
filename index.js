bc01e5d8232e5415927d3602f5d71e56e47697f6
Gauri <Gauri319>
2022-06-29 12:59:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var input=document.getElementById("search");
+const btnxyz = document.querySelector("#showbtn");
+
+function showbtn(){
+    if(input.style.display="none"){
+        input.style.display = "block";
+    }
+}
